46a2fcd123f3a275c8b93ac9981c7068be6822e3
Mickaël Floc’hlay <mickael@flochlay.com>
2018-02-14 17:36:37 +0100

    Add Date extension + Quick spec

---
+++ Example/Tests/DateFlaneurSpec.swift
@@ -0,0 +1,33 @@
+//
+//  DateFlaneurSpec.swift
+//  FlaneurOpen_Tests
+//
+//  Created by Mickaël Floc'hlay on 14/02/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import FlaneurOpen
+
+class DateFlaneurSpec: QuickSpec {
+    override func spec() {
+        describe("Human readable time intervals") {
+            context("when using French locale") {
+                it("won't fail with the now date") {
+                    expect(Date().humanReadableStringForTimeIntervalToNow()).toNot(beNil())
+                }
+
+                it("will work with reference dates") {
+                    let refDate = Date(timeIntervalSince1970: 1518606000)
+
+                    let locale = Locale.current.identifier
+                    debugPrint(locale)
+
+                    expect(Date(timeIntervalSince1970: 373555835).humanReadableStringForTimeIntervalToNow(refDate)).to(equal("About 36 years"))
+                }
+            }
+        }
+    }
+}
+
