012fc686e4e99d6ff974fbc4ad721e4ca3b42159
Mickaël Floc’hlay <mickael@flochlay.com>
2018-04-23 18:11:26 +0200

    Tiny bug fixed found during integration

--- Sources/Classes/Form Tools/FlaneurFormImagePickerElementCollectionViewCell.swift
+++ Sources/Classes/Form Tools/FlaneurFormImagePickerElementCollectionViewCell.swift
@@ -29,7 +29,7 @@ public extension FlaneurFormImagePickerElementCollectionViewCellDelegate where S
     }
 
     func sourceProviders() -> [FlaneurImageProvider] {
-        return []
+        return FlaneurImagePickerConfig.defaultSources
     }
 
     func localizedAttributedStringForTitle() -> NSAttributedString {
